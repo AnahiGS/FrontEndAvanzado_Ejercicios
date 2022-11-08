06545dee0a86d14322cfcf55bd980dd6bfd7f70b
Anahi Garcia Soria <anahi.gsoria@gmail.com>
2022-11-07 19:06:04 -0600

    add pages

---
+++ 02-react-router/src/pages/Contacto.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import logo from '../assets/react.svg'
+
+const Contacto = () => {
+  return (
+    
+    <h1>Contacto</h1>
+  )
+}
+
+export default Contacto
---
+++ 02-react-router/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import logo from '../assets/react.svg'
+
+const Home = () => {
+  return (
+    <>
+      <img src={logo} className='App-logo' alt='logo' />
+      <h1>Home</h1>
+    </>
+  )
+}
+
+export default Home
