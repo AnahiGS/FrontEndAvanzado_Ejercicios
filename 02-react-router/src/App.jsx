ca9c4000e8e764ebc7eab55f8d4d99e99d16024d
Anahi Garcia Soria <anahi.gsoria@gmail.com>
2022-11-07 21:09:50 -0600

    enlaces dinámicos

--- 02-react-router/src/App.jsx
+++ 02-react-router/src/App.jsx
@@ -1,10 +1,12 @@
 
 import './App.css'
+import Navbar from './components/navbar'
 import RoutesIndex from './routes'
 
 function App () {
   return (
     <div className='App'>
+      <Navbar />
       <RoutesIndex />
     </div>
   )
--- 02-react-router/src/pages/Portafolio.jsx
+++ 02-react-router/src/pages/Portafolio.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { Link, Outlet } from 'react-router-dom'
 
 const Portafolio = () => {
   return (
-    <h1>Portafolio</h1>
+    <>
+      <h1>Portafolio</h1>
+      
+      <nav>
+        <ul>
+          <li>
+            <Link to='/portafolio/1'>Pinterest</Link>
+          </li>
+          <li>
+            <Link to='/portafolio/2'>Cajero</Link>
+          </li>
+          <li>
+            <Link to='/portafolio/3'>MovieDB</Link>
+          </li>
+        </ul>
+      </nav>
+      <Outlet />
+    </>
   )
 }
 
